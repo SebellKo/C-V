94fb0f3047d83d38da02b8ae25dbfb6343835d79
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-03-29 22:25:43 +0900

    feat: SelectButton 컴포넌트 html, css 구현

---
+++ c-v/src/components/header/SelectButton.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+import openButtonIcon from '../../assets/images/open-button.svg';
+
+const SelectButtonWrapper = styled.div`
+  display: flex;
+  padding: 5px 10px;
+  gap: 10px;
+  background-color: #45454d;
+  border-radius: 15px;
+  cursor: pointer;
+`;
+
+const ListName = styled.span`
+  color: #fff;
+  font-weight: 500;
+`;
+
+const SelectButton = () => {
+  return (
+    <SelectButtonWrapper>
+      <ListName>New</ListName>
+      <img src={openButtonIcon}></img>
+    </SelectButtonWrapper>
+  );
+};
+
+export default SelectButton;
