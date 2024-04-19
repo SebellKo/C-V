a85632a4a9b1d7fd8820c3d5c6d19a58490f3b0e
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-04-19 22:49:31 +0900

    feat: 스타일 적용 ConfirmButtons 컴포넌트 구현

--- c-v/src/components/Modal/ConfirmButtons.jsx
+++ c-v/src/components/Modal/ConfirmButtons.jsx
@@ -1,5 +1,28 @@
+import styled from 'styled-components';
+
+const ConfirmButtonsWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  > button {
+    width: 40px;
+    font-size: 10px;
+    font-weight: 800;
+    background-color: #414141;
+    border-style: none;
+    border-radius: 10px;
+    color: #fff;
+    padding: 1px 0;
+    cursor: pointer;
+  }
+`;
+
 const ConfirmButtons = () => {
-  return;
+  return (
+    <ConfirmButtonsWrapper>
+      <button>확인</button>
+      <button>취소</button>
+    </ConfirmButtonsWrapper>
+  );
 };
 
 export default ConfirmButtons;
