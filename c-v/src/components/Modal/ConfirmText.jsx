ef7b0f9a982aad75d42eb0bfe9c174477922872b
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-04-19 22:50:00 +0900

    feat: 스타일 적용 ConfirmText 컴포넌트 구현

--- c-v/src/components/Modal/ConfirmText.jsx
+++ c-v/src/components/Modal/ConfirmText.jsx
@@ -1,5 +1,12 @@
+import styled from 'styled-components';
+
+const StyledConfirmText = styled.p`
+  font-size: 10px;
+  font-weight: 600;
+`;
+
 const ConfirmText = () => {
-  return;
+  return <StyledConfirmText>전체 삭제 하시겠습니까 ?</StyledConfirmText>;
 };
 
 export default ConfirmText;
