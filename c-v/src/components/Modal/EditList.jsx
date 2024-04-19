9b89b14213b1a8108f6d3795732423757a360f57
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-04-19 22:50:35 +0900

    feat: 스타일 적용 EditList 컴포넌트 구현

--- c-v/src/components/Modal/EditList.jsx
+++ c-v/src/components/Modal/EditList.jsx
@@ -1,5 +1,39 @@
+import styled from 'styled-components';
+
+import deleteIcon from '../../assets/images/delete-black.svg';
+import dragDropIcon from '../../assets/images/drag-drop.svg';
+import EditInput from './EditInput';
+
+const EditListWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  list-style: none;
+  > li {
+    display: flex;
+    gap: 10px;
+    > img {
+      width: 8px;
+      cursor: pointer;
+    }
+  }
+`;
+
 const EditList = () => {
-  return;
+  return (
+    <EditListWrapper>
+      <li>
+        <img src={dragDropIcon} alt="drag icon" />
+        <EditInput />
+        <img src={deleteIcon} alt="delete icon" />
+      </li>
+      <li>
+        <img src={dragDropIcon} alt="drag icon" />
+        <EditInput />
+        <img src={deleteIcon} alt="delete icon" />
+      </li>
+    </EditListWrapper>
+  );
 };
 
 export default EditList;
