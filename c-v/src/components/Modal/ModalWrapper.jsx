fc2c7ce7fa8c6001c3715ded7ef76fd89ca04c93
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-04-19 22:51:03 +0900

    feat: 스타일 적용 ModalWrapper 컴포넌트 구현

--- c-v/src/components/Modal/ModalWrapper.jsx
+++ c-v/src/components/Modal/ModalWrapper.jsx
@@ -1,5 +1,31 @@
+import styled from 'styled-components';
+import ConfirmButtons from './ConfirmButtons';
+import EditInput from './EditInput';
+import ConfirmText from './ConfirmText';
+import EditList from './EditList';
+
+const StyledModalWrapper = styled.div`
+  width: 230px;
+  padding: 25px 0 15px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  border: 1px solid #414141;
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
 const ModalWrapper = () => {
-  return;
+  return (
+    <StyledModalWrapper>
+      <EditInput></EditInput>
+      <ConfirmText></ConfirmText>
+      <EditList></EditList>
+      <ConfirmButtons></ConfirmButtons>
+    </StyledModalWrapper>
+  );
 };
 
 export default ModalWrapper;
