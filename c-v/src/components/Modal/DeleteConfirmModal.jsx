f5f7cc542deee92bd3598eb600e89fd27bf40ad6
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-11-01 03:20:58 +0900

    [style]: 삭제확인 모달 ui 구현

---
+++ c-v/src/components/Modal/DeleteConfirmModal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import ModalCard from '../../styles/components/ModalCard';
+import ModalTitle from '../../styles/components/ModalTitle';
+import ConfirmButtons from '../../styles/components/ConfirmButtons';
+import Button from '../common/Button';
+import { useListStore } from '../../stores/ListStore';
+import { useDeleteConfirmModalStore } from '../../stores/ModalStore';
+
+function DeleteConfirmModal() {
+  const currentListName = useListStore((state) => state.currentListName);
+  const closeDeleteConfirmModal = useDeleteConfirmModalStore(
+    (state) => state.closeModal,
+  );
+
+  return (
+    <ModalCard>
+      <ModalTitle>"{currentListName}" 리스트를 삭제 하시겠습니까 ?</ModalTitle>
+      <Desc>저장된 모든 커맨드들이 삭제됩니다</Desc>
+      <ConfirmButtons>
+        <Button>확인</Button>
+        <Button onClick={closeDeleteConfirmModal}>취소</Button>
+      </ConfirmButtons>
+    </ModalCard>
+  );
+}
+
+const Desc = styled.p`
+  font-size: 10px;
+  color: #e82a36;
+`;
+
+export default DeleteConfirmModal;
