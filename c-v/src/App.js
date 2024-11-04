226934c71551b62c7f8252dc08ef4f54408dffcc
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-11-05 00:50:56 +0900

    [docs]: react-query 추가

--- c-v/src/App.js
+++ c-v/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
 import GlobalStyles from './styles/GlobalStyle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const AppContainer = styled.div`
   width: 100%;
@@ -14,15 +15,19 @@ const AppContainer = styled.div`
   gap: 15px;
 `;
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <GlobalStyles></GlobalStyles>
-      <AppContainer>
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
-      </AppContainer>
+      <QueryClientProvider client={queryClient}>
+        <AppContainer>
+          <Header></Header>
+          <Main></Main>
+          <Footer></Footer>
+        </AppContainer>
+      </QueryClientProvider>
     </>
   );
 }
