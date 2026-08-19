948802f8f110e9843e204249b4d1e483a1583e7c
SebellKo <79312827+SebellKo@users.noreply.github.com>
2026-08-19 22:09:39 +0900

    build: migrate extension to Vite (#31)

--- src/api/postList.js
+++ src/api/postList.js
@@ -1,11 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
-
 const postList = async (listTitle) => {
   const result = await new Promise((resolve, reject) => {
     chrome.runtime
       .sendMessage({
         type: 'add-list',
-        message: { listName: listTitle, id: uuidv4() },
+        message: { listName: listTitle, id: crypto.randomUUID() },
       })
       .then((response) => {
         resolve(response);
